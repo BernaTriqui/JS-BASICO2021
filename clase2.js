b55095bb33f288ecbc16557f0035345fa6e95455
BernaTriqui <75945406+BernaTriqui@users.noreply.github.com>
2021-08-03 12:55:51 -0500

    se añada la clase 2 -variables:string y funciones

---
+++ clase2.js
@@ -0,0 +1,14 @@
+var nombre = "sachj";
+var apellido = "azaAAza";
+
+var nombreEnMayuscula = nombre.toUpperCase();
+var apellidoEnMinuscula = apellido.toLowerCase();
+
+var primeraLetraDelNombre = nombre.charAt(0);
+
+var ultimaLetraDelNombre = nombre.substr(-1);
+
+var cantidadDeLetrasDelNombre = nombre.length;
+
+var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`;
+var str = nombre.substr(1, 3);
